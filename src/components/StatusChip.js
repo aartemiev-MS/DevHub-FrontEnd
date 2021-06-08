a480b2674170ad3e7af0c8925829869e92e603f8
tymur-MS <tymur@magnificentsystems.com>
2021-06-08 19:01:27 +0300

    Create StatusChip.js

---
+++ src/components/StatusChip.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Chip from '@material-ui/core/Chip';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+
+export default (props) => {
+
+    const getInitials_Short = name => name.split(' ').map(word => word.charAt(0)).join('')
+    const getInitials_Long = name => name.split(' ')[0] + ' ' + name.split(' ').splice(1).map(word => word.charAt(0) + '.').join(' ')
+
+    return props.empty ?
+        <Chip
+            size="small"
+            label='Add'
+            variant="outlined"
+            icon={<AddCircleIcon />}
+            clickable
+            onClick={props.onClick}
+        /> :
+        <Chip
+            size="small"
+            label={props.shortForm ? getInitials_Short(props.dev.name) : getInitials_Long(props.dev.name)}
+            style={{ backgroundColor: props.dev.associatedBackgroundColor, color: props.dev.isWhiteForegroundColor ? 'white' : 'black', cursor: 'pointer' }}
+            clickable={props.clickable}
+            onClick={props.clickable ? props.onClick : undefined}
+        />
+}
