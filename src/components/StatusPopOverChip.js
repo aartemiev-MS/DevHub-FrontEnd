4a1850d7384a1f6fb55b12f50b1b0b3bdb406e03
tymur-MS <tymur@magnificentsystems.com>
2021-06-14 19:05:37 +0300

    Update StatusPopOverChip.js

--- src/components/StatusPopOverChip.js
+++ src/components/StatusPopOverChip.js
@@ -1,63 +1,78 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Popover from '@material-ui/core/Popover';
-import FormGroup from '@material-ui/core/FormGroup';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Popover from "@material-ui/core/Popover";
+import FormGroup from "@material-ui/core/FormGroup";
 import StatusChip from "./StatusChip";
+import Chip from "@material-ui/core/Chip";
 
-import { getTasksData } from '../tableData'
+import { getTasksData } from "../tableData";
 
 const useStyles = makeStyles((theme) => ({
-    typography: {
-        padding: theme.spacing(2),
-    },
+  typography: {
+    padding: theme.spacing(2),
+  },
 }));
 
-export default function StatusPopOverChip(prop) {
-    const [anchorEl, setAnchorEl] = React.useState(null);
+export default function StatusPopOverChip(props) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
-    const statuses = props.statusSource
+  const statuses = props.statusSource;
 
-    const handlePopOverOpen = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
+  const handlePopOverOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-    const handleClose = (e) => {
-        setAnchorEl(null);
-    };
+  const handleClose = (e) => {
+    setAnchorEl(null);
+  };
 
-    const isOpen = Boolean(anchorEl);
+  const isOpen = Boolean(anchorEl);
 
-    return (
-        <span>
-            <DevChip
-                stat={props.stat}
-                clickable={!props.readOnly}
-                onClick={handlePopOverOpen}
-                empty={props.empty}
-            />
-            {!props.readOnly && <Popover
-                open={isOpen}
-                anchorEl={anchorEl}
-                onClose={handleClose}
-                anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'center',
-                }}
-                transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'center',
-                }}
-            >
-                <FormGroup column className='status-popover'>
-                    {statuses.map(status =>
-                        <StatusChip
-                            stat={status}
-                            onClick={e => props.handleStatusSelected(status.id, props.taskId)}
-                            clickable
-                        />)}
-                </FormGroup>
-            </Popover>}
-        </span>
-    );
+  console.log(props.statusId, statuses, statuses.find(s => {
+    const debug = s.id === props.statusId;
+    // debugger
+    return s.id === props.statusId;
+  }));
 
+  return (
+    <span>
+      <Chip
+        label={statuses.find(s => s.id === props.statusId) ? statuses.find(s => s.id === props.statusId).statusName : "not found"}
+        // label={statuses[0].statusName}
+        size="small"
+        className="status-chip"
+        onClick={handlePopOverOpen}
+        //stat={props.stat}
+      />
+      {!props.readOnly && (
+        <Popover
+          open={isOpen}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "center",
+          }}
+        >
+          <FormGroup column className="status-popover">
+            {statuses.filter(s => s.id !== props.taskId).map((status) => (
+              <Chip
+                size="small"
+                stat={status}
+                label={status.statusName}
+                onClick={e =>
+                  props.handleStatusSelected(status.id, props.taskId)
+                }
+                clickable
+              />
+            ))}
+          </FormGroup>
+        </Popover>
+      )}
+    </span>
+  );
 }
